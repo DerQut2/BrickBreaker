6ca9028e8dadd3b35277e3eea099aac8ee74a6a3
DerQut2 <275818@student.pwr.edu.pl>
2025-06-19 00:20:29 +0200

    Added a second OLED, removed redundant casting

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -47,6 +47,7 @@
 ADC_HandleTypeDef hadc1;
 
 I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
 
 /* USER CODE BEGIN PV */
 volatile uint8_t player_pos = 0;
@@ -66,6 +67,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_ADC1_Init(void);
+static void MX_I2C2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -104,15 +106,19 @@ int main(void)
   MX_GPIO_Init();
   MX_I2C1_Init();
   MX_ADC1_Init();
+  MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
-  if (ssd1306_Init(&hi2c1) != 0) {
+  if (ssd1306_Init(&hi2c1) != 0 || ssd1306_Init(&hi2c2) != 0) {
       Error_Handler();
   }
   HAL_Delay(10);
 
 
   blit_splash_screen(&hi2c1);
+  blit_splash_screen(&hi2c2);
+
   ssd1306_UpdateScreen(&hi2c1);
+  ssd1306_UpdateScreen(&hi2c2);
 
   // Start ADC
   HAL_ADC_Start_IT(&hadc1);
@@ -131,7 +137,7 @@ int main(void)
 	  ssd1306_Fill(Black);
 
 	  blit_palette(&hi2c1, (uint8_t) (player_pos), 12);
-	  blit_ball(&hi2c1, (uint8_t) ball_x_pos, (uint8_t) ball_y_pos);
+	  blit_ball(&hi2c1, ball_x_pos, ball_y_pos);
 
 	  ssd1306_UpdateScreen(&hi2c1);
 
@@ -275,6 +281,40 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
 
 }
 
